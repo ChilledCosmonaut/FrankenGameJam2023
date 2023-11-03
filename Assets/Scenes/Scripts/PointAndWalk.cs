d5d0621b17d402fc6929a800f3d94a812469e7b4
Blu3Andy <BlueAndyDev@gmail.com>
2023-11-03 22:57:19 +0100

    brrr

--- Assets/Scenes/Scripts/PointAndWalk.cs
+++ Assets/Scenes/Scripts/PointAndWalk.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PointAndWalk : MonoBehaviour
 {
+    NavMeshAgent _navAgent;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        _navAgent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray =  Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 1000, _navAgent.areaMask))
+            {
+                _navAgent.SetDestination(hit.point);
+            }
+        }
     }
 }
