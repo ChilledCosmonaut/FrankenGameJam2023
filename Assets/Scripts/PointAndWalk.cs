22f8046cf28b03ed5a5e0a527e8709dd6d383a97
Blu3Andy <BlueAndyDev@gmail.com>
2023-11-04 16:09:20 +0100

    new input try

--- Assets/Scripts/PointAndWalk.cs
+++ Assets/Scripts/PointAndWalk.cs
@@ -5,8 +5,11 @@ using UnityEngine.AI;
 
 public class PointAndWalk : MonoBehaviour
 {
+
+    public GameObject cursor;
+    public float speed = 5f;
+
     NavMeshAgent _navAgent;
-    // Start is called before the first frame update
     void Awake()
     {
         _navAgent = GetComponent<NavMeshAgent>();
@@ -15,14 +18,43 @@ public class PointAndWalk : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        _navAgent.SetDestination(cursor.transform.position);
+
+        if(Input.GetKey(KeyCode.W))
+        {
+            MovePlayer(Vector3.up);
+        }
+        
+        if(Input.GetKey(KeyCode.A))
+        {
+            MovePlayer(Vector3.left);
+        }
+
+        if(Input.GetKey(KeyCode.S))
+        {
+            MovePlayer(Vector3.down);
+        }
+
+        if(Input.GetKey(KeyCode.D))
+        {
+            MovePlayer(Vector3.right);
+        }
+
+        /*
         if(Input.GetMouseButtonDown(0))
         {
             RaycastHit hit;
             Ray ray =  Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out hit, 1000, _navAgent.areaMask))
             {
-                _navAgent.SetDestination(hit.point);
+                
             }
         }
+        */
+    }
+
+    private void MovePlayer(Vector3 direction)
+    {
+        cursor.transform.Translate(direction * speed * Time.deltaTime);
     }
 }
