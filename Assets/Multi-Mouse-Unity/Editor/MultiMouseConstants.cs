a0d258c1f4c8ecf57b330dde9fdb6314a48f90a9
Blu3Andy <BlueAndyDev@gmail.com>
2023-11-05 18:19:00 +0100

    THATS IT

--- Assets/Multi-Mouse-Unity/Editor/MultiMouseConstants.cs
+++
@@ -1,9 +0,0 @@
-namespace MultiMouseUnity.Editor
-{
-    public class MultiMouseConstants
-    {
-        public const string PACKAGE_NAME = "Multi Mouse";
-        public const string MENU_TITLE = PACKAGE_NAME + "/";
-        public const string MENU_SETTINGS = "Project/" + PACKAGE_NAME;
-    }
-}
--- Assets/Multi-Mouse-Unity/Examples/Lightgun Example/Scripts/IShootable.cs
+++
@@ -1,4 +0,0 @@
-public interface IShootable
-{
-    void InvokeOnShotBehaviour();
-}
